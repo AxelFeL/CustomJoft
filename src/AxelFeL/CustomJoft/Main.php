f903e2a7b6874784ff81f1d4acff0c2db91444c7
AxelFeL542 <91774548+AxelFeL542@users.noreply.github.com>
2022-01-19 09:44:29 +0700

    Main.php

---
+++ src/AxelFeL/CustomJoft/Main.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AxelFeL\CustomJoft;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\utils\Config;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+
+class Main extends PluginBase implements Listener {
+    
+    public function onEnable(): void{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getLogger("Plugin CustomJoFt Enabled By AxelFeL!");
+        $this->saveResource("config.yml");
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+    }
+    
+    public function onJoin(PlayerJoinEvent $event){
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $rank = $this->getServer()->getPluginManager()->getPlugin("PurePerms")->getUserDataMgr()->getGroup($player)->getName();
+        $event->setJoinMessage(str_replace(["{name}", "{rank}"], [$name, $rank], $this->getConfig()->get("join-message")));
+    }
+    
+    public function onQuit(PlayerQuitEvent $event){
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $rank = $this->getServer()->getPluginManager()->getPlugin("PurePerms")->getUserDataMgr()->getGroup($player)->getName();
+        $event->setQuitMessage(str_replace(["{name}", "{rank}"], [$player, $rank], $this->getConfig()->get("left-message")));
+    }
+}
